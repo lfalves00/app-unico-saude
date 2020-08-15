2cd9511864092d33a6eedc3dc8ad6fb31c4bec1a
Filipe Alves <lf@lcda.com.br>
2020-08-15 15:11:40 -0300

    feature: List specialitys and create local routes connections

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,36 @@
 import express, { request, response } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, res)=>{
-  return response.json({message: 'cole again'});
+routes.get('/specialitys', async (request, response) => {
+  const specialitys = await knex('specialitys').select('*');
+
+  return response.json(specialitys);
+});
+
+routes.post('/locals', async (request, response) => {
+  const {
+    name,
+    email,
+    tellphone,
+    uf,
+    city,
+    hood,
+    specialitys
+  } = request.body; 
+
+  await knex('locals').insert({
+    image: 'image-fake',
+    name,
+    email,
+    tellphone,
+    uf,
+    city,
+    hood
+  });
+
+  return response.json({success: true});
 });
 
 export default routes;
